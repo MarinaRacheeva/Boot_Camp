0ae1c5f81516d8c102cc6680c9ad20f6348b48a4
MarinaRacheeva <68225548+MarinaRacheeva@users.noreply.github.com>
2020-07-22 19:50:01 +0700

    Add files via upload

--- project_Marina_Racheeva/template/assets/js/script.js
+++ project_Marina_Racheeva/template/assets/js/script.js
@@ -1,43 +1,35 @@
 'use strict';
 jQuery(function () {
 
-    $('.buy__quantity-btn-minus').click(function () {
+    $('.buy__quantity-btn').click(function (){
         let $input = $(this).parent().find('input');
-        let $count = parseInt($input.val()) - 1;
-        if ($count < 1) {
-            $count = 1;
+        let sign = $(this).attr('data-name');
+        let count = parseInt($input.val());
+        switch (sign) {
+            case 'minus': {
+                count--;
+                if (count < 1) {
+                    count = 1;
+                }
+            break;
+            }
+            case 'plus': {
+                count++;
+                break;
+            }
         }
-        $input.val($count);
-        $input.change();
-        return false;
-    });
-
-    $('.buy__quantity-btn-plus').click(function () {
-        let $input = $(this).parent().find('input');
-        let $count = parseInt($input.val()) + 1;
-        $input.val($count);
-        $input.change();
-        return false;
+    $input.val(count);
+    return false;
     });
 
     $('.buy__quantity-count').blur(function(){
         let $input = $(this);
-        let $count = $(this).val();
-        if (!$count) {
-            $count = 0;
+        let count = $(this).val();
+        if (!count) {
+            count = 0;
         }
-        $input.val($count);
-        $input.change();
+        $input.val(count);    
     });  
-    
-    if ('devicePixelRatio' in window && window.devicePixelRatio == 2){
-        let $lowresImages = $('img.car__photo-img');
-        images.each(function(i) {
-            let $lowres = $(this).find('img').attr('src');
-            let $highres = $lowres.replace(".", "2x.");
-            $(this).attr('src', $highres);
-        });
-    }
     
 });
 

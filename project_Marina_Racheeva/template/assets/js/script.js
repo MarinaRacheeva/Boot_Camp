1781d9c8b1878585ede0571ff2fc69dcdafd9f71
MarinaRacheeva <68225548+MarinaRacheeva@users.noreply.github.com>
2020-07-22 14:36:46 +0700

    Add files via upload

--- project_Marina_Racheeva/template/assets/js/script.js
+++ project_Marina_Racheeva/template/assets/js/script.js
@@ -1,29 +1,44 @@
 'use strict';
-
 jQuery(function () {
 
     $('.buy__quantity-btn-minus').click(function () {
         let $input = $(this).parent().find('input');
-        let count = parseInt($input.val()) - 1;
-        if (count < 1) {
-            count = 1;
+        let $count = parseInt($input.val()) - 1;
+        if ($count < 1) {
+            $count = 1;
         }
-        $input.val(count);
+        $input.val($count);
         $input.change();
         return false;
     });
 
     $('.buy__quantity-btn-plus').click(function () {
         let $input = $(this).parent().find('input');
-        let count = parseInt($input.val()) + 1;
-        $input.val(count);
+        let $count = parseInt($input.val()) + 1;
+        $input.val($count);
         $input.change();
         return false;
     });
 
-
- 
-     
+    $('.buy__quantity-count').blur(function(){
+        let $input = $(this);
+        let $count = $(this).val();
+        if (!$count) {
+            $count = 0;
+        }
+        $input.val($count);
+        $input.change();
+    });  
+    
+    if ('devicePixelRatio' in window && window.devicePixelRatio == 2){
+        let $lowresImages = $('img.car__photo-img');
+        images.each(function(i) {
+            let $lowres = $(this).find('img').attr('src');
+            let $highres = $lowres.replace(".", "2x.");
+            $(this).attr('src', $highres);
+        });
+    }
+    
 });
 
 
